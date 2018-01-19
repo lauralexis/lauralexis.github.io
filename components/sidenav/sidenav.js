866f2997aeb8681ba63e9b7959867b4baec61ef2
Laura Weiner <laura@gust.com>
2018-01-19 13:52:31 -0500

    WIP

---
+++ components/sidenav/sidenav.js
@@ -0,0 +1,29 @@
+require('./sidenav.scss');
+
+module.exports = ($route, $location) => {
+  return {
+    restrict: 'E',
+    scope: {},
+    template: (require('./sidenav.jade'))(null),
+    link: (scope, elem, attrs) => {
+
+      const components = Object.values($route.routes).filter(route => route.title);
+      const componentDictionary = {};
+
+      components.forEach(component => {
+        componentDictionary[component.title] = component.originalPath;
+      });
+
+      scope.components = components;
+      scope.activeComponent = $route.current.$$route.title;
+      scope.go = (path) => {
+        $location.path(path);
+      };
+
+      scope.setComponent = (componentTitle) => {
+        scope.activeComponent = componentTitle;
+        scope.go(componentDictionary[componentTitle]);
+      };
+    }
+  };
+};
