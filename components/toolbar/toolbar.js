4621c15cc8eb11377ec7b49ced34dc1f3abc70cd
Laura Weiner <laura@gust.com>
2018-01-19 11:00:46 -0500

    WIP

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -7,13 +7,18 @@ module.exports = ($route, $location) => {
     template: (require('./toolbar.jade'))(null),
     link: (scope, elem, attrs) => {
       scope.components = Object.values($route.routes).filter(route => route.title);
+      var reformattedArray = scope.components.map((obj) => {
+         var rObj = {};
+         rObj[obj.title] = obj.originalPath;
+         return rObj;
+      });
       scope.activeComponent = $route.current.$$route.title;
       scope.go = (path) => {
         $location.path(path);
       };
-      scope.setComponent = (component) => {
-        scope.activeComponent = component.title;
-        scope.go(component.originalPath);
+      scope.setComponent = (componentTitle) => {
+        scope.activeComponent = componentTitle;
+        scope.go(reformattedArray[componentTitle].originalPath);
       };
     }
   };
