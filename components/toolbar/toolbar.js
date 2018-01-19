9106c9c5f0c1a0a4f7f41c7a92cbb4eae9f03542
Laura Weiner <laura@gust.com>
2018-01-19 12:01:01 -0500

    WIP

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -8,25 +8,21 @@ module.exports = ($route, $location) => {
     link: (scope, elem, attrs) => {
 
       const components = Object.values($route.routes).filter(route => route.title);
+      const componentDictionary = {};
+
+      components.forEach(component => {
+        componentDictionary[component.title] = component.originalPath;
+      });
+
       scope.components = components;
       scope.activeComponent = $route.current.$$route.title;
       scope.go = (path) => {
         $location.path(path);
       };
 
-      var reformattedArray = Object.values($route.routes)
-      .filter(route => route.title)
-      .map((obj) => {
-         var rObj = {};
-         rObj[obj.title] = obj.originalPath;
-         return rObj;
-      });
       scope.setComponent = (componentTitle) => {
         scope.activeComponent = componentTitle;
-        var path = reformattedArray.filter(e => {
-          return Object.keys(e).toString() === componentTitle;
-        });
-        scope.go(path[0][componentTitle]);
+        scope.go(componentDictionary[componentTitle]);
       };
     }
   };
